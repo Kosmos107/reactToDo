1b74d84d9ccd96e7a954741e95589b9b598381e7
kosmos107 <strateg87@gmail.com>
2021-11-11 11:40:57 +0300

    исправлена ошибка с неправильной передачей пропса

--- src/component/Content/todo/ToDo.jsx
+++ src/component/Content/todo/ToDo.jsx
@@ -2,19 +2,20 @@ import React from "react"
 import s from "./ToDo.module.scss"
 import ToDoList from "./ToDoList"
 const ToDo=(props)=>{
-    console.log(props.info)
-    console.log(props.info)
+    console.log(props)
+    let mapArr = props.tex.map((arr,num)=>{
+        console.log(arr)              
         return(
-        <>
-            <div className={s.wrapperOrigin}>
-                {props.info.map((arr,num)=>{
-                console.log(arr,num)
-                debugger
-                return <ToDoList text={arr} key={num} />
-                })}
+            <ToDoList t={arr.text} key={num} />
+        )
+    })
+        return(
+        <>  
+            <div  className={s.wrapperOrigin}>
+              {mapArr}
             </div>
-            <input onChange={props.funcNew} value={props.newText} className={s.textToDo} />
-            <button onClick={props.funcText} className={s.ToDoButtons} >
+            <input onKeyDown={props.funcKey} onChange={props.funcNew} value={props.newText} className={s.textToDo} />
+            <button  onClick={props.funcText} className={s.ToDoButtons} >
                 add
             </button>
         </>
--- src/component/Content/todo/ToDoList.jsx
+++ src/component/Content/todo/ToDoList.jsx
@@ -3,15 +3,19 @@ import s from "./ToDo.module.scss"
 
 
 function ToDoList(props){
-    
+    console.log(props)
+    console.log(props.t)
     return(
         <label className={s.wrapper} >
             <input className={s.ListInput}  type="checkbox" />
-            <p className={s.text}>
-                {props.text}
-            </p>
+            <div className={s.text}>
+                {props.t}
+            </div>
             
         </label>
     )
 }
+//разабраться в этом пиздеце
+//исправить проблему с пустым массивом
+
 export default ToDoList
